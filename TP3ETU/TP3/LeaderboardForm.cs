e8ff665765c7cf045b6bfc40afa7c1fc7fc0a879
Charlesfire <charlesfire2@hotmail.com>
2015-05-19 09:59:46 -0400

    Revert "Correction validation nom"
    This reverts commit 4882f17725e0559781cd866d745001b3ad98e3ee.

--- TP3ETU/TP3/LeaderboardForm.cs
+++ TP3ETU/TP3/LeaderboardForm.cs
@@ -9,14 +9,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TP3
 {
   public partial class LeaderboardForm : Form
   {
+    string texteFichier = "";
+    string[] currentScores = new string[10];
+    int[] leaderBoardScoresNumbers = new int[11];
     public LeaderboardForm()
     {
       InitializeComponent();
+      //<Tommy Bouffard>
+      try
+      {
+        texteFichier = File.ReadAllText("Leaderboard.txt");
+        currentScores = texteFichier.Split(';');
+        for (int i = 0; i != currentScores.Length; i++)
+        {
+          string[] division = currentScores[i].Split(',');
+          leaderBoardScoresNumbers[i] = int.Parse(currentScores[0]);
+        }
+        if (MillipedeGame.Score < leaderBoardScoresNumbers[9])
+        {
+
+        }
+      }
+      catch (Exception ex)
+      {
+        System.Console.WriteLine("LeaderBoard: " + ex.Message);
+      }
+      //</Tommy Bouffard>
+    }
+    /*
+     *   
+  procédure tri_selection(tableau t, entier n)
+      pour i de 1 à n - 1
+          min ← i
+          pour j de i + 1 à n
+              si t[j] < t[min], alors min ← j
+          fin pour
+          si min ≠ i, alors échanger t[i] et t[min]
+      fin pour
+  fin procédure
+    */
+    /// <summary>
+    /// Cette finction trie les valeurs du leaderboard.
+    /// </summary>
+    /// <returns></returns>
+    public int[] SortScores()
+    {
+      return new int[0];
+    }
+    public void GetLeaderBoard()
+    {
+
     }
 
     private void btnQuitter_Click(object sender, EventArgs e)
@@ -31,7 +79,8 @@ namespace TP3
 
     private void btnValider_Click(object sender, EventArgs e)
     {
-      if ((txtbNom.Text = txtbNom.Text.Trim()).Length != 0)
+      txtbNom.Text = txtbNom.Text.Trim();
+      if (txtbNom.Text != "")
       {
         pnlEntrerNom.Visible = false;
         trvMeilleursScores.Nodes.Add("exemple");
@@ -41,18 +90,6 @@ namespace TP3
     private void trvMeilleursScores_BeforeSelect(object sender, TreeViewCancelEventArgs e)
     {
       e.Cancel = true;
-    }
-
-    private void txtbNom_TextChanged(object sender, EventArgs e)
-    {
-      if (Char.IsLetterOrDigit(txtbNom.Text[txtbNom.Text.Length - 1]))
-      {
-        btnValider.Enabled = true;
-      }
-      else
-      {
-        btnValider.Enabled = false;
-      }
     }
   }
 }
