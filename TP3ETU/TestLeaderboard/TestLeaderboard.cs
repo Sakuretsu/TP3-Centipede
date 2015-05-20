54c0f82184f9f4ea662a6614601210266f0c1131
Sakuretsu <korvax1@gmail.com>
2015-05-20 17:35:11 -0400

    Tests leaderboard (manque certain tests)

--- TP3ETU/TestLeaderboard/TestLeaderboard.cs
+++ TP3ETU/TestLeaderboard/TestLeaderboard.cs
@@ -15,27 +15,44 @@ namespace TestLeaderboard
     //(Le test devrait échouer car il n'y devrait pas avoir d'exception)
     public void TestChargementFichier()
     {
-      LeaderboardForm board = new LeaderboardForm(0);
+      LeaderboardForm board = new LeaderboardForm();
     }
     [TestMethod]
     //Vérification de la capacité à intégrer un score au milieu du tableau (on suppose que le tableau est à l'état initial
-    //10e place = 2 et 9e place = 23) 
+    //10e place = 2 et 9e place = 23)
     public void TestEntreeAuMilieu()
     {
-      LeaderboardForm board = new LeaderboardForm(20);
+      LeaderboardForm board = new LeaderboardForm();
+      board.ScoreTester = 50;
       board.SortScores();
-      int[] boardAfterSort = board.GetLeaderBoard();
-      Assert.AreEqual(boardAfterSort[9],20);
+      Assert.AreEqual(board.GetLeaderBoard()[7], 50);
     }
     [TestMethod]
     //Vérifie si un score inférieur à la derniere entrée peut se retrouver dans le tableau
-    public void TestNotBestScore()
+    public void TestMiddleScore()
     {
-      LeaderboardForm board = new LeaderboardForm(-1);
-      board.SortScores();
+      LeaderboardForm board = new LeaderboardForm();
+      board.ScoreTester = -1;
       //L'algorithme ne touchera pas au fichier en écriture si il n'est pas plus grand que le dixième élément.
-      int[] sortedBoard = board.GetLeaderBoard();
-      Assert.AreEqual(sortedBoard[10], -1);
+      Assert.AreEqual(board.GetLeaderBoard()[10], -1);
+    }
+    [TestMethod]
+    //Vérifie si un pointage peut être le meilleur
+    public void TestBestScore()
+    {
+      LeaderboardForm board = new LeaderboardForm();
+      board.ScoreTester = 666;
+      board.SortScores();
+      Assert.AreEqual(board.GetLeaderBoard()[0], 666);
+    }
+    //Vérifie si un pointage peut être le dixième (dernier).
+    [TestMethod]
+    public void TestLastScore()
+    {
+      LeaderboardForm board = new LeaderboardForm();
+      board.ScoreTester = 1;
+      board.SortScores();
+      Assert.AreEqual(board.GetLeaderBoard()[9], 1);
     }
   }
 }
